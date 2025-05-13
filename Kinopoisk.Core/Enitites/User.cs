dddf42dd1fd15ff3ac39d590ee6ac3c36c2deb13
Nariman <narbiiaz@gmail.com>
2025-05-13 10:42:23 +0600

    Added Context and User

--- Kinopoisk.Core/Enitites/User.cs
+++ Kinopoisk.Core/Enitites/User.cs
@@ -1,6 +1,15 @@
-﻿namespace Kinopoisk.Core.Enitites;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class User
+namespace Kinopoisk.Core.Enitites;
+
+public class User : IdentityUser<int>
 {
+    public List<Comment> Comments { get; set; }
+    public List<Rating> Ratings { get; set; }
 
+    public User()
+    {
+        Comments = new();
+        Ratings = new();
+    }
 }
---
+++ Kinopoisk.DataAccess/KinopoiskContext.cs
@@ -0,0 +1,27 @@
+﻿using Kinopoisk.Core.Enitites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kinopoisk.DataAccess;
+
+public class KinopoiskContext : IdentityDbContext
+{
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<ActorRole> ActorRoles { get; set; }
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Film> Films { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public KinopoiskContext(DbContextOptions opts) : base(opts)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
